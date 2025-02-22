1e67871cbb3e31bb895dc673eae3c9c4ba4ed77c
TrkGbr <145534543+TrkGbr@users.noreply.github.com>
2025-02-22 16:13:15 +0100

    Create Course.cs

---
+++ Course.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Academy_2025.Data
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
