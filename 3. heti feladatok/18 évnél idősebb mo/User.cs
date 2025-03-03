f8e27dab246916b5d8e76021c7ee86ccf4638b74
TrkGbr <145534543+TrkGbr@users.noreply.github.com>
2025-03-03 18:17:14 +0100

    User.cs Age property kiegészítés

---
+++ 3. heti feladatok/18 évnél idősebb mo/User.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Academy_2025.Data
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? LastName { get; set; }
+
+        [Required]
+        public int Age { get; set; } //<-------------Age property kiegészítés
+    }
+}
