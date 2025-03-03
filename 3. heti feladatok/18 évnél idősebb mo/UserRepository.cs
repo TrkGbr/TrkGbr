6dcd8ca146d48b06681e3ddb91c0d466d88cd0d9
TrkGbr <145534543+TrkGbr@users.noreply.github.com>
2025-03-03 18:14:05 +0100

    UserRepository kiegészítés megoldással

---
+++ 3. heti feladatok/18 évnél idősebb mo/UserRepository.cs
@@ -0,0 +1,67 @@
+using Academy_2025.Data;
+
+namespace Academy_2025.Respositories
+{
+    public class UserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public List<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public List<User> GetUsersOver18()  //<---------------MEGOLDÁS
+        {
+            var nameQuery = _context.Users.Where(user => user.Age > 18);
+
+            return nameQuery.ToList();
+        }
+
+        public User? GetById(int id)
+        {
+            return _context.Users.FirstOrDefault(user => user.Id == id);
+        }
+
+        public void Create(User data)
+        {
+            _context.Users.Add(data);
+            _context.SaveChanges();
+        }
+
+        public User? Update(int id, User data)
+        {
+            var user = _context.Users.FirstOrDefault(user => user.Id == id);
+            if (user != null)
+            {
+                user.FirstName = data.FirstName;
+                user.LastName = data.LastName;
+                user.Age = data.Age;
+                _context.SaveChanges();
+
+                return user;
+            }
+
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            var user = _context.Users.FirstOrDefault(user => user.Id == id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
