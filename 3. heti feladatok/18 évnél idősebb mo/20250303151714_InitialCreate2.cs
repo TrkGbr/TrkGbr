06eb3acbfdb096ed939208a74cef6a68d51f5358
TrkGbr <145534543+TrkGbr@users.noreply.github.com>
2025-03-03 18:20:48 +0100

    új migráció

---
+++ 3. heti feladatok/18 évnél idősebb mo/20250303151714_InitialCreate2.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Academy_2025.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate20 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Users");
+        }
+    }
+}
